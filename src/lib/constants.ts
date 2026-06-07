4fba1545f9ef89415f0f83251610203143a873ea
John Di Donna <didonna97john@gmail.com>
2026-06-07 23:36:09 +0000

    The OMDb API <donotreply@omdbapi.com> 7:33 PM (0 minutes ago) to me
    Her

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,8 +5,7 @@
  * Es necesario confirmar el email que te envían para activar la clave.
  */
 
-// NOTA: Si ves un error 401, es que esta clave ha caducado o llegado a su límite.
-// Por favor, genera una propia en el enlace de arriba y cámbiala aquí.
-export const OMDB_API_KEY = process.env.NEXT_PUBLIC_OMDB_API_KEY || '6453000b'; 
+// Tu clave API activada: dba6be57
+export const OMDB_API_KEY = process.env.NEXT_PUBLIC_OMDB_API_KEY || 'dba6be57'; 
 
 export const OMDB_BASE_URL = 'https://www.omdbapi.com/';
